788807b3bbf0ac63ea612a5fede4666c43d40ad4
praveen.x.fuloria <praveen.x.fuloria@haleon.com>
2024-06-11 13:04:05 +0530

    Add project files.

---
+++ api_versioning_Demo/Controllers/EmployeeV1Controller.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace api_versioning_Demo.Controllers
+{
+    [Route("api/employee")]
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Route("api/{v:apiVersion}/employee")]
+    public class EmployeeV1Controller : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new OkObjectResult("employees from v1 controller");
+        }
+
+    }
+}
---
+++ api_versioning_Demo/Controllers/EmployeeV2Controller.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace api_versioning_Demo.Controllers
+{
+    [Route("api/employee")]
+    [ApiController]
+    [ApiVersion("2.0")]
+    [Route("api/{v:apiVersion}/employee")]
+    public class EmployeeV2Controller : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new OkObjectResult("employees from v2 controller");
+        }
+    }
+}
